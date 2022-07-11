7cb68aaa128722a5fedcc49743d9774289d00d5e
brnkgabriel <brnkgabriel@gmail.com>
2022-07-11 14:30:12 +0100

    to fix media queries in NextJS

--- comps/Header.jsx
+++ comps/Header.jsx
@@ -6,6 +6,7 @@ const Header = ({ name, description, url }) => {
     <Head>
       <title>{name}</title>
       <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <meta property="og:title" content="DT-Equip | Design, Develop & Animate" />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={"https://dt-equip.vercel.app/" + url} />
--- comps/Navbar.jsx
+++ comps/Navbar.jsx
@@ -6,13 +6,15 @@ const Navbar = () => {
       <div className="logo">
         <h1>DT-Equip</h1>
       </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/design"><a>Design</a></Link>
-      <Link href="/develop"><a>Develop</a></Link>
-      <Link href="/animate"><a>Animate</a></Link>
-      <Link href="/about"><a>About</a></Link>
+      <div className="links">
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/design"><a>Design</a></Link>
+        <Link href="/develop"><a>Develop</a></Link>
+        <Link href="/animate"><a>Animate</a></Link>
+        <Link href="/about"><a>About</a></Link>
+      </div>
     </nav>
   );
 }
- 
+
 export default Navbar;
